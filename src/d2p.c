c51fe50ef2a9bdaf43a04593b9981632623ea077
Thomas Seignette <tseignette@enseirb-matmeca.fr>
2018-10-30 16:56:43 -0400

    Version parallèle fonctionnelle

--- src/d2p.c
+++ src/d2p.c
@@ -3,48 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-int U[100000000];
-int V[100000000];
+int T[1000000000];
+int A[1000000000];
 
-int recherche_dichotomique(int valeur_recherchee, int tab[], int ind_debut, int ind_fin) {
-	int ind_milieu;
-	while (ind_debut <= ind_fin) {
-		ind_milieu = (ind_fin + ind_debut) / 2;
-		if (tab[ind_milieu] == valeur_recherchee)
-			return ind_milieu;
-		else if (valeur_recherchee > tab[ind_milieu])
-			ind_debut = ind_milieu + 1;
-		else
-			ind_fin = ind_milieu - 1;
-	}
-	return -1;
+void interchanger(int *a, int *b) {
+	int a_tmp = *a;
+	int b_tmp = *b;
+	*a = b_tmp;
+	*b = a_tmp;
 }
 
-void fusion(int T[], int p1, int r1, int A[], int p2, int r2, int p3) {
+int recherche_dichotomique(int valeur_recherchee, int tab[], int ind_debut, int ind_fin) {
+	if(ind_debut > ind_fin) ind_fin = ind_debut;
+
+	while(ind_debut < ind_fin) {
+		int ind_milieu = (ind_debut + ind_fin) / 2;
+
+		if(valeur_recherchee <= tab[ind_milieu])
+			ind_fin = ind_milieu;
+		else
+			ind_debut = ind_milieu + 1;
+	}
+
+	return ind_fin;
+}
+
+void fusion(int T[], int p1, int r1, int p2, int r2, int A[], int p3) {
 	int n1 = r1 - p1 + 1;
 	int n2 = r2 - p2 + 1;
 
 	//interchanger (n1,p1,r1) et (n2,p2,r2) 
-	if (n1 < n2) {
-		int temp = n1;
-		n1 = n2;
-		n2 = temp;
-		temp = p1;
-		p1 = p2;
-		p2 = temp;
-		temp = r1;
-		r1 = r2;
-		r2 = temp;
+	if(n1 < n2) {
+		interchanger(&n1, &n2);
+		interchanger(&p1, &p2);
+		interchanger(&r1, &r2);
 	}
 
-	if (n1 > 0) {
+	if(n1 > 0) {
 		int q1 = (p1 + r1) / 2;
 		int q2 = recherche_dichotomique(T[q1], T, p2, r2);
-		if (q2 == -1) return; // Erreur
 		int q3 = p3 + (q1 - p1) + (q2 - p2);
 		A[q3] = T[q1];
-		fusion(T, p1, q1 - 1, A, p2, q2 - 1, p3);
-		fusion(T, q1 + 1, r1, A, q2, r2, q3 + 1);
+		#pragma omp task
+		fusion(T, p1, q1 - 1, p2, q2 - 1, A, p3);
+		#pragma omp task
+		fusion(T, q1 + 1, r1, q2, r2, A, q3 + 1);
 	}
 }
 
@@ -79,31 +82,30 @@ int main() {
     // Initialisation des tableaux
     int offset = 0;
     int n = get_next_number(file, &offset);
-    int m = n;
 
-    for(int i = 0; i < n; i++) U[i] = get_next_number(file, &offset);
-    for(int i = 0; i < n; i++) V[i] = get_next_number(file, &offset);
+    for(int i = 0; i < 2 * n; i++) T[i] = get_next_number(file, &offset);
 
-		printf("U : ");
-		for(int i = 0; i < n; i++)
-			printf("%d ", U[i]);
-		printf("\n");
+		// printf("T1 : ");
+		// for(int i = 0; i < n; i++)
+		// 	printf("%d ", T[i]);
+		// printf("\n");
 
-		printf("V : ");
-		for(int i = 0; i < m; i++)
-			printf("%d ", V[i]);
-		printf("\n");
+		// printf("T2 : ");
+		// for(int i = n; i < 2 * n; i++)
+		// 	printf("%d ", T[i]);
+		// printf("\n");
 
-    // Fusion séquentielle
+    // Fusion parallèle
     double start = omp_get_wtime();
-		fusion(U, 0, 1, V, 0, 1, 0);
+		#pragma omp parallel
+		fusion(T, 0, n - 1, n, 2 * n, A, 0);
     double end = omp_get_wtime();
 
-		// Affichage du résultat
-		printf("V : ");
-		for(int i = 0; i < n + m; i++)
-			printf("%d ", V[i]);
-		printf("\n");
+		// // Affichage du résultat
+		// printf("A : ");
+		// for(int i = 0; i < 2 * n; i++)
+		// 	printf("%d ", A[i]);
+		// printf("\n");
 
     // Affichage du temps
     printf("%f\n", end - start);
