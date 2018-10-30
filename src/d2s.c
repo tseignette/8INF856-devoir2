65b7c484d1b6068a98491c075f37db952664c5f8
Thomas Seignette <tseignette@enseirb-matmeca.fr>
2018-10-29 22:37:20 -0400

    Conversion fichier vers tableau faite pour la version séquentielle

--- src/d2s.c
+++ src/d2s.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void fusion(int U[], int n, int V[], int m, int T[]){
 	int i = 0;
@@ -23,32 +24,65 @@ void fusion(int U[], int n, int V[], int m, int T[]){
 	}
 }
 
-int main() {
-  // Initialisation des tableaux
-  int n = 13;
-	int U[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-  int m = 13;
-	int V[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+int get_next_number(char *input, int *offset) {
+  input += *offset;
 
-  printf("U : ");
-  for(int i = 0; i < n; i++)
-    printf("%d ", U[i]);
-  printf("\n");
+  int length = 0;
+  while((*(input + length) != ' ') && (*(input + length) != '\0')) length++;
 
-  printf("V : ");
-  for(int i = 0; i < m; i++)
-    printf("%d ", V[i]);
-  printf("\n");
+  char n[length + 1];
+  n[length] = '\0';
+  strncpy(n, input, length);
+  *offset += length + 1;
 
-  // Fusion séquentielle
-	int T[n + m];
-	fusion(U, n, V, m, T);
+  return atoi(n);
+}
 
-  // Affichage du résultat
-  printf("T : ");
-	for(int i = 0; i < n + m; i++)
-		printf("%d ", T[i]);
-  printf("\n");
+int main(int argc, char *argv[]) {
+  // On lit le fichier test
+  char *file = 0;
+  long length;
+
+  if(stdin) {
+    fseek(stdin, 0, SEEK_END);
+    length = ftell(stdin);
+    fseek(stdin, 0, SEEK_SET);
+    file = malloc(length);
+    if(file) fread(file, 1, length, stdin);
+  }
+
+  if(file) {
+    // Initialisation des tableaux
+    int offset = 0;
+    int n = get_next_number(file, &offset);
+
+    int m = n;
+    int U[n];
+    int V[n];
+
+    for(int i = 0; i < n; i++) U[i] = get_next_number(file, &offset);
+    for(int i = 0; i < n; i++) V[i] = get_next_number(file, &offset);
+
+    printf("\nU : ");
+    for(int i = 0; i < n; i++)
+      printf("%d ", U[i]);
+    printf("\n");
+
+    printf("\nV : ");
+    for(int i = 0; i < m; i++)
+      printf("%d ", V[i]);
+    printf("\n");
+
+    // Fusion séquentielle
+    int T[n + m];
+    fusion(U, n, V, m, T);
+
+    // Affichage du résultat
+    printf("\nT : ");
+    for(int i = 0; i < n + m; i++)
+      printf("%d ", T[i]);
+    printf("\n");
+  }
 
 	return EXIT_SUCCESS;
 }
